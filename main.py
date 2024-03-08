78fb1ab79aa9e0db062fcc9c2a4e2cb805ea917c
stephane-rbn <rabena.stephane@gmail.com>
2024-03-08 22:48:31 +0900

    Split schema into input and output + add delete method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
-from schemas import load_db
+from schemas import WasteMaterialInput, WasteMaterialOutput, load_db, save_db
 
 app = FastAPI()
 
@@ -24,5 +24,28 @@ def get_waste_material_by_id(id: int) -> dict:
     for waste_material in db:
         if waste_material.id == id:
             return waste_material.dict()
-
     raise HTTPException(status_code=404, detail=f"No waste material found with id={id}")
+
+
+@app.post("/api/waste-materials", response_model=WasteMaterialOutput)
+def add_waste_material(waste_material: WasteMaterialInput) -> WasteMaterialOutput:
+    new_waste_material = WasteMaterialOutput(
+        id=len(db) + 1,
+        name_en=waste_material.name_en,
+        name_kr=waste_material.name_kr,
+        description=waste_material.description,
+        recyclable=waste_material.recyclable,
+    )
+    db.append(new_waste_material)
+    save_db(db)
+    return new_waste_material
+
+
+@app.delete("/api/waste-materials/{id}", status_code=204)
+def delete_waste_material(id: int) -> None:
+    for waste_material in db:
+        if waste_material.id == id:
+            db.remove(waste_material)
+            save_db(db)
+            return
+    raise HTTPException(status_code=404, detail=f"No waste material with id={id}")
--- schemas.py
+++ schemas.py
@@ -3,15 +3,18 @@ import json
 from pydantic import BaseModel
 
 
-class WasteMaterial(BaseModel):
-    id: int
+class WasteMaterialInput(BaseModel):
     name_en: str
     name_kr: str
     description: str | None = "Todo"
     recyclable: bool | None = True
 
 
-def load_db() -> list[WasteMaterial]:
+class WasteMaterialOutput(WasteMaterialInput):
+    id: int
+
+
+def load_db() -> list[WasteMaterialOutput]:
     """Load a list of WasteMaterial objects from a JSON file"""
     with open("waste-materials.json") as f:
         json_data = json.load(f)
@@ -19,7 +22,18 @@ def load_db() -> list[WasteMaterial]:
         waste_materials = []
 
         for obj in json_data:
-            waste_material = WasteMaterial.model_validate(obj)
+            waste_material = WasteMaterialOutput.model_validate(obj)
             waste_materials.append(waste_material)
 
         return waste_materials
+
+
+def save_db(waste_materials: list[WasteMaterialInput]) -> None:
+    """Save a list of new WasteMaterial objects in JSON file"""
+    with open("waste-materials.json", "w") as f:
+        converted_dictionaries = []
+
+        for waste_material in waste_materials:
+            converted_dictionaries.append(waste_material.dict())
+
+        json.dump(converted_dictionaries, f, indent=4)
