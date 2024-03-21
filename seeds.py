5f16c61af24c6c56ca88a93be9ed8f85734693a1
stephane-rbn <rabena.stephane@gmail.com>
2024-03-21 18:34:15 +0900

    Add db seeding file: only bins

---
+++ seeds.py
@@ -0,0 +1,57 @@
+import os
+
+from faker import Faker
+from sqlmodel import Session
+
+from config import get_session
+from core.models.bin import Bin
+from core.schemas.bin import BinInput
+from core.utils.slug_utils import SlugUtils
+
+BIN_NAMES_DICT = {
+    "General Waste": "일반쓰레기",
+    "PET bottles": "PET병",
+    "Clear plastic bottle": "투명 페트병",
+    "Color plastic bottle": "유색 페트병",
+    "Plastic": "플라스틱류",
+    "Vinyl": "비닐류",
+    "Can": "캔류",
+    "Paper": "종이류",
+    "Glass bottle": "유리병",
+    "Food waste": "음식물 쓰레기",
+    "Waste battery collection box": "전지류 (폐건전지 수거함)",
+    "Recyclable waste": "재활용 쓰레기",
+    "Place directly on the ground next to the recycling bins (no plastic bag)": "재활용통 옆에 플라스틱 봉투 안이 아니라 바닥에 직접 둠",
+    "Report to the local district office": "구청에 신고함",
+    "Report to the local community center": "주민센터에 신고함",
+}
+
+fake = Faker()
+fake_ko = Faker("ko_KR")
+
+
+class Seeds:
+    @staticmethod
+    def load_fake_data() -> None:
+        session: Session = next(get_session())
+
+        for name_en, name_kr in BIN_NAMES_DICT.items():
+            bin_name_en = name_en
+            bin_name_kr = name_kr
+            bin_slug = SlugUtils.generate_slug(name_en)
+            bin_description = fake.text()
+
+            bin_instance = BinInput(
+                name_en=bin_name_en, name_kr=bin_name_kr, description=bin_description
+            )
+
+            new_bin = Bin.model_validate(bin_instance, update={"slug": bin_slug})
+
+            session.add(new_bin)
+            session.commit()
+            session.refresh(new_bin)
+
+
+if __name__ == "__main__":
+    if eval(os.getenv("ENV_TEST").lower().capitalize()):  # type: ignore  # noqa: S307
+        Seeds.load_fake_data()
