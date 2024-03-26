3f1aee426d0e64fb54c42aa7ba9feece5cc06d67
stephane-rbn <rabena.stephane@gmail.com>
2024-03-26 21:26:32 +0900

    Update seeds.py: now seed wm table too

--- seeds.py
+++ seeds.py
@@ -1,11 +1,13 @@
 import os
+import random
 
 from faker import Faker
 from sqlmodel import Session
 
 from config import get_session
 from core.models.bin import Bin
-from core.schemas.bin import BinInput
+from core.models.waste_material import WasteMaterial
+from core.schemas.waste_material import WasteMaterialInput
 from core.utils.slug_utils import SlugUtils
 
 BIN_NAMES_DICT = {
@@ -41,15 +43,47 @@ class Seeds:
             bin_slug = SlugUtils.generate_slug(name_en)
             bin_description = fake.text()
 
-            bin_instance = BinInput(
-                name_en=bin_name_en, name_kr=bin_name_kr, description=bin_description
+            bin_instance = Bin(
+                name_en=bin_name_en,
+                name_kr=bin_name_kr,
+                description=bin_description,
+                slug=bin_slug,
             )
 
-            new_bin = Bin.model_validate(bin_instance, update={"slug": bin_slug})
+            new_bin = Bin.model_validate(bin_instance)
 
             session.add(new_bin)
             session.commit()
             session.refresh(new_bin)
+
+            waste_materials_count = 20
+
+            for _ in range(waste_materials_count):
+                random_name_en = " ".join(fake.words(3)).capitalize()
+
+                waste_material_name_en = random_name_en
+                waste_material_name_kr = fake_ko.catch_phrase()
+                waste_material_slug = SlugUtils.generate_slug(random_name_en)
+                waste_material_description = fake.text()
+                waste_material_recycle = bool(random.getrandbits(1))
+
+                waste_material_instance = WasteMaterialInput(
+                    name_en=waste_material_name_en,
+                    name_kr=waste_material_name_kr,
+                    description=waste_material_description,
+                    recyclable=waste_material_recycle,
+                )
+
+                new_waste_material = WasteMaterial.model_validate(
+                    waste_material_instance, update={"slug": waste_material_slug}
+                )
+
+                # Purposely not assigning last waste material to any bin
+                if _ < (waste_materials_count - 1):
+                    new_bin.waste_materials.append(new_waste_material)
+
+                session.add(new_waste_material)
+                session.commit()
 
 
 if __name__ == "__main__":
