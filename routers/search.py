8f78c2345ab8b156eb064712368d20ab5bedf7ab
stephane-rbn <rabena.stephane@gmail.com>
2024-04-05 20:49:41 +0900

    Add limit rate of 20 requests per second for waste material search

--- routers/search.py
+++ routers/search.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Request
 from sqlmodel import Session
 
 from config import get_session
 from core.schemas.waste_material import WasteMaterialSearchResult
 from core.utils.search_utils import SearchUtils
 
+from .limiter import limiter
+
 router = APIRouter(prefix="/api/v1/search")
 
 
 @router.get("/", response_model=list[WasteMaterialSearchResult])
+@limiter.limit("20/second")
 def search_waste_materials(
+    request: Request,
     q: str = Query(None, description="Search waste material"),
     session: Session = Depends(get_session),
 ) -> list[WasteMaterialSearchResult]:
