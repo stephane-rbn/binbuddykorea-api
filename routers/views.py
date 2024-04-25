415221e117145bdd5c6ccbf86ba1e5ad89ffcf46
stephane-rbn <rabena.stephane@gmail.com>
2024-04-25 10:13:39 +0900

    Change templates extension from html to jinja

--- routers/views.py
+++ routers/views.py
@@ -8,4 +8,4 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("layout.jinja", {"request": request})
