ddbe54cabc7ebb31afef17e7fbd283cc34ad6403
stephane-rbn <rabena.stephane@gmail.com>
2024-04-25 22:34:20 +0900

    Apply prettier on main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
-const searchInput = document.getElementById('searchInput');
-const searchResults = document.getElementById('searchResults');
+const searchInput = document.getElementById("searchInput");
+const searchResults = document.getElementById("searchResults");
 
-searchInput.addEventListener('input', async (event) => {
+searchInput.addEventListener("input", async (event) => {
   const query = event.target.value.trim();
   if (query.length === 0) {
-    searchResults.innerHTML = ''; // Clear search results if search input is empty
+    searchResults.innerHTML = ""; // Clear search results if search input is empty
     return;
   }
 
@@ -13,17 +13,17 @@ searchInput.addEventListener('input', async (event) => {
   const data = await response.json();
 
   // Clear previous search results
-  searchResults.innerHTML = '';
+  searchResults.innerHTML = "";
 
   // Display search results
-  data.forEach(result => {
-    const listItem = document.createElement('li');
+  data.forEach((result) => {
+    const listItem = document.createElement("li");
     listItem.innerHTML = `${result.name_en} - (${result.name_kr}) - ${highlightMatch(result.description, query)}`;
     searchResults.appendChild(listItem);
   });
 });
 
 function highlightMatch(text, query) {
-  const regex = new RegExp(`(${query})`, 'gi');
+  const regex = new RegExp(`(${query})`, "gi");
   return text.replace(regex, '<span class="highlight">$1</span>');
 }
